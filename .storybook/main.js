3b8815a88cfaaa885510efaeae435ead2e19dc44
rms-shinse <shinse_tanaka@recruit-ms.co.jp>
2021-05-07 19:23:53 +0900

    feat(storybook): angular12 uses webpack5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,10 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    "../projects/ui/src/lib/stories/**/*.stories.mdx",
+    "../projects/ui/src/lib/stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  core: {
+    builder: "webpack5",
+  },
+};
