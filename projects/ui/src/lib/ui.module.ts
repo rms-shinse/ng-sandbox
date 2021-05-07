ec85f56004364e739f3543a723bb72f9fa35946e
rms-shinse <shinse_tanaka@recruit-ms.co.jp>
2021-05-07 15:08:30 +0900

    feat(ui) generate UI Library
    ```
    yarn run ng g library ui --prefix=ui
    yarn lint --fix
    ```

---
+++ projects/ui/src/lib/ui.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { UiComponent } from './ui.component';
+
+@NgModule({
+  declarations: [UiComponent],
+  imports: [],
+  exports: [UiComponent],
+})
+export class UiModule {}
