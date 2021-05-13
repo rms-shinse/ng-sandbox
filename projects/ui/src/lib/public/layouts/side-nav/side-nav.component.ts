9ec54920c4828dbc235e291aaac068f150393ba7
rms-shinse <shinse_tanaka@recruit-ms.co.jp>
2021-05-13 10:55:59 +0900

    fix(angular): angular-cli/issues/20726 fixed in ng-packager@12.0.0-rc2
    https://github.com/angular/angular-cli/issues/20726

--- projects/ui/src/lib/public/layouts/side-nav/side-nav.component.ts
+++ projects/ui/src/lib/public/layouts/side-nav/side-nav.component.ts
@@ -3,18 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'ui-side-nav',
   templateUrl: './side-nav.component.html',
-  // FIXME: When use scss, ng build frozen.
-  // https://github.com/angular/angular-cli/issues/20726
-  // styleUrls: ['./side-nav.component.scss'],
-  styles: [
-    `
-      :host {
-        display: block;
-        background-color: #eee;
-        position: fixed;
-      }
-    `,
-  ],
+  styleUrls: ['./side-nav.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SideNavComponent {
